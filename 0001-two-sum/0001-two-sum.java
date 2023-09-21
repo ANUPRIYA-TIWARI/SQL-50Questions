651301bad9ce9ddf9da5008356c6381c00a8eaae
ANUPRIYA <88017965+ANUPRIYA-TIWARI@users.noreply.github.com>
2023-09-21 20:51:14 +0530

    Time: 46 ms (25.03%), Space: 43.5 MB (84.34%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,9 +1,9 @@
 //  O(n^2)time  O(1)space
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        if (nums != null && nums.length > 1) {
+      
             for (int i = 0; i < nums.length; i++){
-            for(int j = i+1; j < nums.length ; j++)
+            for(int j = i+1; j < nums.length ; j++){
             if (nums[i] + nums[j] == target){
                         int a[] = {i, j};
                         return a;
@@ -14,4 +14,5 @@ class Solution {
         return null;
         
         }
-    }
+}
+    
